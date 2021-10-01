9775863d6e0be95f21c46cff565e43f53aef8dca
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2021-10-01 20:34:34 +0200

    Cleanup

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -41,8 +41,6 @@ export default defineComponent({
     }
   },
 })
-// https://geodienste.hamburg.de/wms_hvv?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&STYLES=&CRS=EPSG:3857&WIDTH=460&HEIGHT=601&BBOX=1112835.6428758772,7080557.005608099,1120233.342437191,7090222.260904511
-// https://map.geofox.de/geoserver/geofox_workspace/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&BBOX=562394,5932816,564194,5934616&WIDTH=256&HEIGHT=256&STYLES=&CRS=EPSG:25832&LAYERS=geofox_workspace:dynamic_stations,geofox_workspace:geofoxdb_bundr,geofox_workspace:geofoxdb_cambio,geofox_workspace:geofoxdb_europcar,geofox_workspace:geofoxdb_pundr,geofox_workspace:geofoxdb_servicestellen,geofox_workspace:geofoxdb_stations,geofox_workspace:geofoxdb_strecken_nacht,geofox_workspace:geofoxdb_strecken_normal,geofox_workspace:geofoxdb_strecken_sonderfahrplan,geofox_workspace:geofoxdb_strecken_we_nacht,geofox_workspace:geofoxdb_switchh_points,geofox_workspace:geofoxdb_zahlgrenzen,geofox_workspace:geofoxdb_zahlgrenzen_gedehnt,geofox_workspace:geofoxdb_zahlgrenzen_gedehnt_nacht,geofox_workspace:geofoxdb_zahlgrenzen_gedehnt_normal,geofox_workspace:geofoxdb_zahlgrenzen_gedehnt_sonderfahrplan,geofox_workspace:geofoxdb_zahlgrenzen_gedehnt_we_nacht,geofox_workspace:geofoxdb_zahlgrenzen_nacht,geofox_workspace:geofoxdb_zahlgrenzen_normal,geofox_workspace:geofoxdb_zahlgrenzen_sonderfahrplan,geofox_workspace:geofoxdb_zahlgrenzen_we_nacht
 </script>
 
 <template>
