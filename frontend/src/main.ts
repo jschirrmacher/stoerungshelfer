74f3ba4b093b53c9a22bc4921be3e86fd7ef0163
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2021-10-01 10:39:45 +0200

    Adds openlayers and github actions for github pages deployment

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import { createRouter, createWebHashHistory } from "vue-router"
 import MainPage from "./components/MainPage.vue"
 import store from './store'
+import OpenLayersMap from 'vue3-openlayers'
+import 'vue3-openlayers/dist/vue3-openlayers.css'
 
 const routes = [
   { path: "/", component: MainPage },
@@ -16,5 +18,6 @@ const app = createApp(App)
 
 app.use(store)
 app.use(router)
+app.use(OpenLayersMap)
 
 app.mount('#app')
